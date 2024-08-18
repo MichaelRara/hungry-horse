5bc965c93f0f2b4c15287d230e104fcf4dc20724
Michael Rára <michaelrara24@gmail.com>
2024-08-18 16:33:31 +0200

    Chessboard added
    Chessboard has its own class.
    Drawing of path added.

---
+++ chessboard.py
@@ -0,0 +1,110 @@
+from typing import Dict
+
+from PIL import Image
+
+
+class Chessboard:
+
+    def __init__(self, width: int = 8, height: int = 8, step_cost: int = -1) -> None:
+        self.width = width
+        self.height = height
+        self.step_cost = step_cost
+        self.states_rewards = self._create_rectangle_chessboard()
+
+    def _create_rectangle_chessboard(self) -> Dict[tuple[int, int], int]:
+        """Create rectangular chessboard with defined width and height.
+        Chessboard is represented as a dictionary of tuples which consist
+        of coordinates (x, y) and value which represent cost of step.
+
+        Returns:
+            Dict[tuple[int, int], int]: Created chessboard with cost of step.
+        """
+        chessboard = {}
+        for i in range(0, self.width):
+            for j in range(0, self.height):
+                chessboard[(i, j)] = self.step_cost
+        return chessboard
+
+    def modify_chessboard(self) -> None:
+        """Manual modification of step cost for selected states. User can modify this method by his will.
+        Be aware to modify states which are included in chessboard!
+        """
+        #  deadly states
+        self.states_rewards[(5, 5)] = -100
+        self.states_rewards[(5, 6)] = -100
+        self.states_rewards[(6, 5)] = -100
+        self.states_rewards[(6, 6)] = -100
+        self.states_rewards[(8, 4)] = -100
+        self.states_rewards[(8, 5)] = -100
+        self.states_rewards[(9, 4)] = -100
+        self.states_rewards[(9, 5)] = -100
+
+        #  positive states
+        for i in range(0, self.width):
+            self.states_rewards[(i, 7)] = -100
+        for i in range(0, self.height):
+            self.states_rewards[(4, i)] = -100
+        self.states_rewards[(8, 1)] = -100
+        self.states_rewards[(8, 2)] = -100
+        self.states_rewards[(8, 8)] = -100
+        self.states_rewards[(8, 9)] = -100
+        self.states_rewards[(8, 10)] = -100
+        self.states_rewards[(8, 11)] = -100
+        self.states_rewards[(8, 12)] = -100
+        self.states_rewards[(8, 13)] = -100
+        self.states_rewards[(8, 14)] = -100
+        self.states_rewards[(9, 6)] = -100
+
+        #  unfriendly states
+        self.states_rewards[(3, 2)] = -100
+        self.states_rewards[(3, 3)] = -100
+        self.states_rewards[(3, 5)] = -100
+        self.states_rewards[(7, 2)] = -100
+        self.states_rewards[(13, 12)] = -100
+
+    def draw_users_chessboard(self) -> None:
+        """Draw chessboard modified by user and save it as "Chessboard.jpg".
+        """
+        img_of_standard_chessboard = self.draw_standard_chessboard()
+        self.img_of_modified_chessboard = self._draw_modified_chessboard(img_of_standard_chessboard)
+        self.img_of_modified_chessboard.resize(
+                                        size=(1_000, 1_000),
+                                        resample=Image.NEAREST).transpose(Image.FLIP_TOP_BOTTOM).save("Chessboard.jpg")
+
+    def draw_standard_chessboard(self) -> Image:
+        """Draw standard chessboard.
+
+        Returns:
+            Image: Image of standard chessboard.
+        """
+        img_of_standard_chessboard = Image.new(mode="RGB", size=(self.width, self.height))
+        pixels = img_of_standard_chessboard.load()
+        color = [(0, 0, 0), (255, 255, 255)]
+        actual_default_color = True
+        first_color = actual_default_color
+        for i in range(0, self.width):
+            first_color = actual_default_color
+            for j in range(0, self.height):
+                pixels[i, j] = color[actual_default_color]
+                actual_default_color = not actual_default_color
+            actual_default_color = not first_color
+        return img_of_standard_chessboard
+
+    def _draw_modified_chessboard(self, img_of_standard_chessboard: Image) -> Image:
+        """Redraw standard chessboard according to users setting of rewards.
+
+        Args:
+            img_of_standard_chessboard (Image): Image of standard chessboard.
+
+        Returns:
+            Image: Image of modified chessboard.
+        """
+        pixels = img_of_standard_chessboard.load()
+        for state, reward in list(self.states_rewards.items()):
+            if reward == -100:
+                pixels[state[0], state[1]] = (255, 0, 0)
+            elif reward == 5:
+                pixels[state[0], state[1]] = (0, 0, 255)
+            elif reward == -3:
+                pixels[state[0], state[1]] = (255, 0, 255)
+        return img_of_standard_chessboard
--- hungry_searching_horse.py
+++ hungry_searching_horse.py
@@ -1,23 +1,39 @@
 from typing import Dict, List
 
 import pandas as pd
+import pickle
 import numpy as np
 
+from chessboard import Chessboard
+from PIL import Image
 
-class HungryHorse:
 
-    def __init__(self, chessboard, starting_state: tuple[int, int] = (0, 0),
-                 alpha: float = 0.1, eps: float = 0.05) -> None:
+class HungrySearchingHorse:
+
+    def __init__(self,
+                 chessboard: Chessboard,
+                 starting_state: tuple[int, int] = (0, 0),
+                 alpha: float = 0.01,
+                 eps: float = 0.1,
+                 gamma: float = 0.9) -> None:
+        """
+        Args:
+            chessboard (Chessboard): Instance of a class Chessboard.
+            starting_state (tuple[int, int], optional): Starting position of a horse. Defaults to (0, 0).
+            alpha (float, optional): Gradient step parameter. Defaults to 0.1.
+            eps (float, optional): Parameter for epsilon greedy search. Defaults to 0.05.
+            gamma (float, optional): Factor to multiply Q(state, value) of next state-action pair. Defaults to 0.9.
+        """
         self.chessboard = chessboard
         self.starting_state = starting_state
-        self.visited_states = []
+        self.terminal_state = (self.chessboard.width-1, self.chessboard.height-1)
+        self.chessboard.states_rewards[self.terminal_state] = 10
         self.possible_actions = self._generate_possible_actions()
         self.states_actions_values = self._create_combinations_of_all_possible_states_and_actions()
 
         self.alpha = alpha
         self.eps = eps
-        self.gamma = 0.9
-        self.reward = 1
+        self.gamma = gamma
 
     def _generate_possible_actions(self) -> Dict[str, np.ndarray[int, int]]:
         """Create dictionary of possible actions.
@@ -53,7 +69,7 @@ class HungryHorse:
             Dict[tuple[int, int], Dict[str, float]]: All Q(state, action) values initialized to be zero.
         """
         states_actions_values = {}
-        for state in self.chessboard:
+        for state in list(self.chessboard.states_rewards.keys()):
             valid_actions = {}
             for action_name, action_vector in list(self.possible_actions.items()):
                 if self._action_valid(state, action_vector):
@@ -63,7 +79,6 @@ class HungryHorse:
 
     def _action_valid(self, state: tuple[int, int], action_vector: np.ndarray[int, int]) -> bool:
         """Check if action can be done in provided state.
-        If final state is included in chessboard and not included in visited_states return True else False.
 
         Args:
             state (tuple[int, int]): Starting state on a chessboard.
@@ -72,25 +87,28 @@ class HungryHorse:
         Returns:
             bool: If action is valid return True else False.
         """
-        final_state = tuple((np.array(state) + action_vector).tolist())
-        if final_state in self.chessboard and final_state not in self.visited_states:
+        next_state = tuple((np.array(state) + action_vector).tolist())
+        if next_state in list(self.chessboard.states_rewards.keys()):
             return True
         return False
 
-    def run_sarsa(self):
+    def run_sarsa(self) -> None:
         episode_score = {}
-        for episode_number in range(0, 100_000):
+        for episode_number in range(0, 10_000):
+            print("episode number " + str(episode_number))
             actual_state = self.starting_state
             names_of_possible_actions = self._find_possible_actions(actual_state)
+            step = 0
             while names_of_possible_actions is not []:
-                self.visited_states.append(actual_state)
                 name_of_actual_action = self._choose_action_by_greedy_method(actual_state, names_of_possible_actions)
-
-                next_state = tuple((np.array(actual_state) + self.possible_actions[name_of_actual_action]).tolist())
-                names_of_possible_next_actions = self._find_possible_actions(next_state)
-                if names_of_possible_next_actions == []:
-                    self._update_value_of_last_state_and_action(actual_state, name_of_actual_action)
+                next_state = self._calc_next_state(actual_state, name_of_actual_action)
+                if next_state == self.terminal_state:
+                    self._update_state_action_values(actual_state, name_of_actual_action,
+                                                     next_state, name_of_next_action="dl")
+                    episode_score[episode_number] = step
                     break
+
+                names_of_possible_next_actions = self._find_possible_actions(next_state)
                 name_of_next_action = self._choose_action_by_greedy_method(next_state, names_of_possible_next_actions)
 
                 self._update_state_action_values(actual_state, name_of_actual_action, next_state, name_of_next_action)
@@ -98,10 +116,10 @@ class HungryHorse:
                 actual_state = next_state
                 name_of_actual_action = name_of_next_action
                 names_of_possible_actions = names_of_possible_next_actions
-            episode_score[episode_number] = len(self.visited_states)
-            self.visited_states = []
-        pd.DataFrame.from_dict(episode_score, orient='index', columns=["states_visited"]).to_excel("Summary.xlsx")
-        print("The end!")
+                step += 1
+        with open('state_action_values.pickle', 'wb') as handle:
+            pickle.dump(self.states_actions_values, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pd.DataFrame.from_dict(episode_score, orient='index', columns=["Steps_in_episode"]).to_excel("Summary.xlsx")
 
     def _find_possible_actions(self, actual_state: tuple[int, int]) -> List[str]:
         """Detect all possible actions for actual_state.
@@ -161,14 +179,17 @@ class HungryHorse:
         names_of_possible_actions.remove(name_of_best_action)
         return names_of_possible_actions[np.random.randint(0, len(names_of_possible_actions))]
 
-    def _update_value_of_last_state_and_action(self, actual_state: tuple[int, int], name_of_actual_action: str) -> None:
-        """Update Q(state, action) values for sub-last state and its action.
+    def _calc_next_state(self, current_state: tuple[int, int], name_of_current_action: str) -> tuple[int, int]:
+        """Calculate position of next state.
 
         Args:
-            actual_state (tuple[int, int]): Actual state.
-            name_of_actual_action (str): Name of action in current state.
+            current_state (tuple[int, int]): Coordinates of current state.
+            name_of_current_action (str): Name of current action taken in current state.
+
+        Returns:
+            tuple[int, int]: Coordinates of next state.
         """
-        self.states_actions_values[actual_state][name_of_actual_action] += self.alpha * self.reward
+        return tuple((np.array(current_state) + self.possible_actions[name_of_current_action]).tolist())
 
     def _update_state_action_values(self, actual_state: tuple[int, int], name_of_actual_action: str,
                                     next_state: tuple[int, int], name_of_next_action: str) -> None:
@@ -181,35 +202,46 @@ class HungryHorse:
             name_of_next_action (str): Name of action in next state.
         """
         self.states_actions_values[actual_state][name_of_actual_action] += self.alpha * (
-            self.reward
+            self.chessboard.states_rewards[next_state]
             + self.gamma * self.states_actions_values[next_state][name_of_next_action]
             - self.states_actions_values[actual_state][name_of_actual_action])
 
+    def create_animation_of_solution(self):
+        img_of_users_chessboard = self.chessboard.img_of_modified_chessboard
+        pixels = img_of_users_chessboard.load()
+        color_of_actual_state = (255, 128, 0)  # Orange
+        color_of_next_state = (178, 102, 255)  # Purple
+        pixels[self.starting_state] = color_of_actual_state
 
-def create_rectangle_chessboard(width: int = 8, height: int = 8) -> List[tuple[int, int]]:
-    """Create rectangular chessboard with defined width and height.
-    Chessboard is represented as a list of tuples which consist of coordinates (x, y)
+        name_of_actual_action = self._select_best_action(self.starting_state,
+                                                         list(self.states_actions_values[self.starting_state].keys()))
+        actual_state = self.starting_state
+        step = 0
+        while actual_state != self.terminal_state:
+            next_state = self._calc_next_state(actual_state, name_of_actual_action)
 
-    Args:
-        width (int, optional): Width of chessboard. Defaults to 8.
-        height (int, optional): Height of chessboard. Defaults to 8.
+            pixels[actual_state] = color_of_actual_state
+            pixels[next_state] = color_of_next_state
+            img_of_users_chessboard.resize(size=(1_000, 1_000),
+                          resample=Image.NEAREST).transpose(Image.FLIP_TOP_BOTTOM).save("Chessboard_"+str(step)+".jpg")
 
-    Returns:
-        List[tuple[int, int]]: Created chessboard.
-    """
-    chessboard = []
-    for i in range(0, width):
-        for j in range(0, height):
-            chessboard.append((i, j))
-    return chessboard
+            actual_state = next_state
+            name_of_actual_action = self._select_best_action(actual_state,
+                                                             list(self.states_actions_values[actual_state].keys()))
+            step += 1
 
 
 def main() -> None:
-    chessboard = create_rectangle_chessboard()
-    hungry_horse = HungryHorse(chessboard)
+    chessboard = Chessboard(15, 15)
+    chessboard.modify_chessboard()
+    chessboard.draw_users_chessboard()
+
+    hungry_horse = HungrySearchingHorse(chessboard)
     hungry_horse.run_sarsa()
-    pass
+    hungry_horse.create_animation_of_solution()
 
 
 if __name__ == "__main__":
+    np.random.seed(0)
     main()
+    print("Script run successfully!")
